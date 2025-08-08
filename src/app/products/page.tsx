b4b5fe590d60b70f5c30c83f1f913c8fb0c7ab3b
Henry - <henry@Mac.attlocal.net>
2025-08-07 22:05:41 -0400

    Updated logo

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,19 +4,31 @@ export default function ProductsPage() {
       <section className="max-w-4xl mx-auto">
         <h1 className="text-4xl font-bold mb-10 text-center">Our Products</h1>
         <p className="text-lg text-center mb-6">
-          Our models communicate via WiFi or LTE connection depending on your needs. Price includes hardware, installation, and setup.
+          Our models communicate via WiFi or 5G Ultra Capability connection depending on your needs.
         </p>
 
         <ul className="space-y-8">
           <li>
-            <h2 className="text-xl font-semibold">QG-DA-W</h2>
-            <p>Compact Gateway box that taps into your digital and analog signals inside your machine and displays KPIs on your dashboards. Uses pre-existing plant WiFi connection ONLY.
+            <h2 className="text-xl font-semibold">Q-DC-W</h2>
+            <p>Compact Gateway control panel that accepts 24V DC wiring for field signals. Uses pre-existing plant WiFi connection ONLY.
               *Contact for pricing and availability*
             </p>
           </li>
           <li>
-            <h2 className="text-xl font-semibold">QG-DA-5G</h2>
-            <p>Compact Gateway box that taps into your digital and analog signals inside your machine to display KPIs on your dashboard. Includes Wi-Fi and 5G UC connection via data plans.
+            <h2 className="text-xl font-semibold">Q-DC-5G</h2>
+            <p>Compact Gateway control panel that accepts 24V DC wiring for field signals. Includes Wi-Fi and 5G UC connection via data plans.
+              *Contact for pricing and availability*
+            </p>
+          </li>
+                    <li>
+            <h2 className="text-xl font-semibold">Q-AC-W</h2>
+            <p>Compact Gateway control panel that accepts 120v AC wiring for field signals. Uses pre-existing plant WiFi connection ONLY.
+              *Contact for pricing and availability*
+            </p>
+          </li>
+                    <li>
+            <h2 className="text-xl font-semibold">Q-AC-5G</h2>
+            <p>Compact Gateway control panel that accepts 120v AC wiring for field signals. Includes Wi-Fi and 5G UC connection via data plans.
               *Contact for pricing and availability*
             </p>
           </li>
